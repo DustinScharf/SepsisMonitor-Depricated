68e749f0df419142cb75a6d2838511ea82f71ad0
DustinScharf <d.scharf.2002@gmail.com>
2021-11-09 23:30:36 +0100

    Added data interfaces

---
+++ app/src/main/java/com/dustinscharf/sepsismonitor/data/IDataAccess.java
@@ -0,0 +1,21 @@
+package com.dustinscharf.sepsismonitor.data;
+
+import com.dustinscharf.sepsismonitor.util.ICallback;
+
+import java.util.Map;
+
+public interface IDataAccess {
+    public void fetchContainer(String containerKey, ICallback<Map<String, Object>> callback);
+
+    public void fetchContainerItem(String containerKey, String itemKey, ICallback<Map<String, Object>> callback);
+
+    public void subscribeContainer(String containerKey, ICallback<Map<String, Object>> callback);
+
+    public void subscribeContainerItem(String containerKey, String itemKey, ICallback<Map<String, Object>> callback);
+
+    public void addItemToContainer(String containerKey, Map<String, Object> addItem);
+
+    public void editItemInContainer(String containerKey, String itemKey, Map<String, Object> editItem);
+
+    public void removeItemFromContainer(String containerKey, String itemKey);
+}
