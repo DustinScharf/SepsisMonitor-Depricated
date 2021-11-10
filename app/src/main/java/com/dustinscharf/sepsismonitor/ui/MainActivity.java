c8f685b57972be16e8e806a6afd4a42e1e0cc2b3
DustinScharf <d.scharf.2002@gmail.com>
2021-11-10 19:09:46 +0100

    Created Main Activity

--- app/src/main/java/com/dustinscharf/sepsismonitor/ui/MainActivity.java
+++ app/src/main/java/com/dustinscharf/sepsismonitor/ui/MainActivity.java
@@ -1,10 +1,18 @@
 package com.dustinscharf.sepsismonitor.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.dustinscharf.sepsismonitor.R;
+import com.dustinscharf.sepsismonitor.logic.IHospital;
+import com.dustinscharf.sepsismonitor.util.ICallback;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button toLoginButton = findViewById(R.id.toLoginButton);
+        toLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+    }
+
+    private void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
