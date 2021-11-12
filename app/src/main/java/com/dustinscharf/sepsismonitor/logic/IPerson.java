08461093fe56db23629e1be516a5f4bfb7b09787
DustinScharf <d.scharf.2002@gmail.com>
2021-11-12 05:39:56 +0100

    Removed old files

--- app/src/main/java/com/dustinscharf/sepsismonitor/logic/IPerson.java
+++
@@ -1,9 +0,0 @@
-package com.dustinscharf.sepsismonitor.logic;
-
-import com.dustinscharf.sepsismonitor.util.ICallback;
-
-public interface IPerson {
-    public void getFirstName(ICallback<String> callback);
-
-    public void getLastName(ICallback<String> callback);
-}
