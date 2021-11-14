350c75a990a2f9c9ec9023cb7de7e8d7771ec67a
DustinScharf <d.scharf.2002@gmail.com>
2021-11-14 18:51:23 +0100

    Added dark background when pop ups are open

--- app/src/main/java/com/dustinscharf/sepsismonitor/ui/StartActivity.java
+++ app/src/main/java/com/dustinscharf/sepsismonitor/ui/StartActivity.java
@@ -1,84 +1,75 @@
 package com.dustinscharf.sepsismonitor.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.dustinscharf.sepsismonitor.R;
 import com.dustinscharf.sepsismonitor.logic.IHospital;
 import com.dustinscharf.sepsismonitor.ui.adapters.PatientsRecyclerViewAdapter;
-import com.dustinscharf.sepsismonitor.util.ICallback;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This activity contains info about patients and a control panel for operations on patients
+ */
 public class StartActivity extends AppCompatActivity {
+    /**
+     * The id of the the clicked patient
+     */
+    public static final String EXTRA_ID = "com.dustinscharf.sepsismonitor.ui.StartActivity.EXTRA_ID";
+
     private IHospital hospital;
 
     private RecyclerView patientsRecyclerView;
 
     private CardView popUpPatientOverviewCardView;
+    private Button popUpAssignPatientToStaffButton;
     private TextView popUpPatientNameTextView;
     private Button popUpPatientToNextPhaseButton;
-    private Button popUpAssignPatientToStaffButton;
 
     private TextView popUpMultipleNextPhasesTextView;
     private Button popUpPatientToAdmissionPhaseButton;
     private Button popUpPatientToERSepsisTriagePhaseButton;
 
-    private CardView assignStaffToPatientCardView;
-    private EditText enterStaffIdEditText;
-
     private FloatingActionButton addPatientFloatingActionButton;
+
     private CardView addNewPatientCardView;
     private EditText enterNewPatientIdEditText;
     private EditText enterNewPatientFirstNameEditText;
     private EditText enterNewPatientLastNameEditText;
+    private Button addPatientButton;
+
+    private CardView assignStaffToPatientCardView;
+    private EditText enterStaffIdEditText;
+    private Button assignStaffToPatientButton;
 
     private String lastSelectedPatientId;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
-
+    private void initializeFields() {
         this.hospital = IHospital.getNewInstance();
 
         this.patientsRecyclerView = findViewById(R.id.patientsRecyclerView);
-        PatientsRecyclerViewAdapter patientsRecyclerViewAdapter = new PatientsRecyclerViewAdapter(this, hospital);
-        this.hospital.subscribeHospital(new ICallback<Map<String, Object>>() {
-            @Override
-            public void onCallback(Map<String, Object> hospitalMap) {
-                Map<String, Object> patientsMap = (Map<String, Object>) hospitalMap.get("patients");
-                if (hospital.loggedInStaffIsLMMP()) {
-                    patientsRecyclerViewAdapter.setPatients(hospital.getPatients(patientsMap));
-                } else {
-                    patientsRecyclerViewAdapter.setPatients(hospital.getPatientsByStaff(hospitalMap, hospital.getLoggedInStaffId()));
-                }
-            }
-        });
-        patientsRecyclerView.setAdapter(patientsRecyclerViewAdapter);
-        patientsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         this.popUpPatientOverviewCardView = findViewById(R.id.popUpPatientOverviewCardView);
+        this.popUpAssignPatientToStaffButton = findViewById(R.id.popUpAssignPatientToStaffButton);
         this.popUpPatientNameTextView = findViewById(R.id.popUpPatientNameTextView);
         this.popUpPatientToNextPhaseButton = findViewById(R.id.popUpPatientToNextPhaseButton);
 
-        this.popUpAssignPatientToStaffButton = findViewById(R.id.popUpAssignPatientToStaffButton);
         this.popUpMultipleNextPhasesTextView = findViewById(R.id.popUpMultipleNextPhasesTextView);
         this.popUpPatientToAdmissionPhaseButton = findViewById(R.id.popUpPatientToAdmissionPhaseButton);
         this.popUpPatientToERSepsisTriagePhaseButton = findViewById(R.id.popUpPatientToERSepsisTriagePhaseButton);
-
-        this.assignStaffToPatientCardView = findViewById(R.id.assignStaffToPatientCardView);
-        this.enterStaffIdEditText = findViewById(R.id.enterStaffIdEditText);
 
         this.addPatientFloatingActionButton = findViewById(R.id.addPatientFloatingActionButton);
 
@@ -86,110 +77,132 @@ public class StartActivity extends AppCompatActivity {
         this.enterNewPatientIdEditText = findViewById(R.id.enterNewPatientIdEditText);
         this.enterNewPatientFirstNameEditText = findViewById(R.id.enterNewPatientFirstNameEditText);
         this.enterNewPatientLastNameEditText = findViewById(R.id.enterNewPatientLastNameEditText);
+        this.addPatientButton = findViewById(R.id.addPatientButton);
 
-        this.popUpPatientOverviewCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hidePatientOverview();
+        this.assignStaffToPatientCardView = findViewById(R.id.assignStaffToPatientCardView);
+        this.enterStaffIdEditText = findViewById(R.id.enterStaffIdEditText);
+        this.assignStaffToPatientButton = findViewById(R.id.assignStaffToPatientButton);
+
+        this.lastSelectedPatientId = null;
+    }
+
+    /**
+     * Builds the initial gui for this activity
+     *
+     * @param savedInstanceState the data from events happened before the start of this activity
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        this.initializeFields();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(hospital.loggedInStaffIsLMMP() ? "All Patients" : "Your Patients");
+
+        PatientsRecyclerViewAdapter patientsRecyclerViewAdapter = new PatientsRecyclerViewAdapter(this, hospital);
+        this.hospital.subscribeHospital(hospitalMap -> {
+            Map<String, Object> patientsMap = (Map<String, Object>) hospitalMap.get("patients");
+            if (hospital.loggedInStaffIsLMMP()) {
+                patientsRecyclerViewAdapter.setPatients(hospital.getPatients(patientsMap));
+            } else {
+                patientsRecyclerViewAdapter.setPatients(hospital.getPatientsByStaff(hospitalMap, hospital.getLoggedInStaffId(), false));
+            }
+        });
+        patientsRecyclerView.setAdapter(patientsRecyclerViewAdapter);
+        patientsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        this.popUpPatientOverviewCardView.setOnClickListener(v -> popUpPatientOverviewCardView.setVisibility(View.GONE));
+
+        if (hospital.loggedInStaffIsLMMP()) {
+            this.popUpAssignPatientToStaffButton.setText(R.string.assign);
+        } else {
+            this.popUpAssignPatientToStaffButton.setText(R.string.remove);
+        }
+        this.popUpAssignPatientToStaffButton.setOnClickListener(v -> {
+            if (hospital.loggedInStaffIsLMMP()) {
+                assignStaffToPatientCardView.setVisibility(View.VISIBLE);
+            } else {
+                hospital.removePatientFromStaff(lastSelectedPatientId, hospital.getLoggedInStaffId());
             }
         });
 
-        this.popUpPatientToNextPhaseButton.setOnClickListener(new View.OnClickListener() {
+        this.popUpPatientToNextPhaseButton.setOnClickListener(v -> hospital.fetchSinglePatient(lastSelectedPatientId, patientMap -> {
+            int currentPhaseId = Integer.parseInt(patientMap.get("phase").toString());
+            if (currentPhaseId == 1) {
+                popUpMultipleNextPhasesTextView.setVisibility(View.VISIBLE);
+                popUpPatientToAdmissionPhaseButton.setVisibility(View.VISIBLE);
+                popUpPatientToERSepsisTriagePhaseButton.setVisibility(View.VISIBLE);
+            } else {
+                hospital.putPatientIntoPhase(lastSelectedPatientId, currentPhaseId + 1);
+            }
+        }));
+
+        this.popUpPatientToAdmissionPhaseButton.setOnClickListener(v -> {
+            hospital.putPatientIntoPhase(lastSelectedPatientId, 4);
+            popUpMultipleNextPhasesTextView.setVisibility(View.GONE);
+            popUpPatientToAdmissionPhaseButton.setVisibility(View.GONE);
+            popUpPatientToERSepsisTriagePhaseButton.setVisibility(View.GONE);
+        });
+
+        this.popUpPatientToERSepsisTriagePhaseButton.setOnClickListener(v -> {
+            hospital.putPatientIntoPhase(lastSelectedPatientId, 2);
+            popUpMultipleNextPhasesTextView.setVisibility(View.GONE);
+            popUpPatientToAdmissionPhaseButton.setVisibility(View.GONE);
+            popUpPatientToERSepsisTriagePhaseButton.setVisibility(View.GONE);
+        });
+
+
+        this.addPatientFloatingActionButton.setOnClickListener(v -> addNewPatientCardView.setVisibility(View.VISIBLE));
+
+        this.addNewPatientCardView.setOnClickListener(v -> addNewPatientCardView.setVisibility(View.GONE));
+        this.addPatientButton.setOnClickListener(v -> {
+            // TODO check if patient id exists already
+            String newPatientIdInput = enterNewPatientIdEditText.getText().toString().toLowerCase();
+            String newPatientFirstNameInput = enterNewPatientFirstNameEditText.getText().toString();
+            String newPatientLastNameInput = enterNewPatientLastNameEditText.getText().toString();
+            if (Math.min(Math.min(newPatientIdInput.length(), newPatientFirstNameInput.length()), newPatientLastNameInput.length()) > 0) {
+                hospital.addPatient(newPatientIdInput, newPatientFirstNameInput, newPatientLastNameInput);
+                if (!hospital.loggedInStaffIsLMMP()) {
+                    hospital.assignPatientToStaff(newPatientIdInput, hospital.getLoggedInStaffId());
+                }
+                addNewPatientCardView.setVisibility(View.GONE);
+            } else {
+                Toast.makeText(
+                        v.getContext(),
+                        "Please enter every field",
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+        });
+
+        this.assignStaffToPatientCardView.setOnClickListener(v -> assignStaffToPatientCardView.setVisibility(View.GONE));
+        this.assignStaffToPatientButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                hospital.fetchSinglePatient(lastSelectedPatientId, new ICallback<Map<String, Object>>() {
-                    @Override
-                    public void onCallback(Map<String, Object> patientMap) {
-                        int currentPhaseId = Integer.parseInt(patientMap.get("phase").toString());
-                        if (currentPhaseId == 1) {
-                            popUpMultipleNextPhasesTextView.setVisibility(View.VISIBLE);
-                            popUpPatientToAdmissionPhaseButton.setVisibility(View.VISIBLE);
-                            popUpPatientToERSepsisTriagePhaseButton.setVisibility(View.VISIBLE);
-                        } else {
-                            hospital.putPatientIntoPhase(lastSelectedPatientId, currentPhaseId + 1);
-                        }
+            public void onClick(View view) {
+                String inputStaffId = enterStaffIdEditText.getText().toString().toLowerCase();
+                hospital.fetchStaff(stringObjectMap -> {
+                    if (stringObjectMap.containsKey(inputStaffId) && ((Map<String, Object>) stringObjectMap.get(inputStaffId)).containsKey("patients")) {
+                        hospital.assignPatientToStaff(lastSelectedPatientId, inputStaffId);
+                        assignStaffToPatientCardView.setVisibility(View.GONE);
+                    } else {
+                        Toast.makeText(
+                                view.getContext(),
+                                "The staff does not exists or can not get patients assigned",
+                                Toast.LENGTH_LONG
+                        ).show();
                     }
                 });
             }
         });
-
-        this.popUpAssignPatientToStaffButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (assignStaffToPatientCardView.getVisibility() == View.GONE) {
-                    assignStaffToPatientCardView.setVisibility(View.VISIBLE);
-                } else {
-                    String inputStaffId = enterStaffIdEditText.getText().toString();
-                    hospital.fetchStaff(new ICallback<Map<String, Object>>() {
-                        @Override
-                        public void onCallback(Map<String, Object> stringObjectMap) {
-                            if (stringObjectMap.containsKey(inputStaffId) && ((Map<String, Object>) stringObjectMap.get(inputStaffId)).containsKey("patients")) {
-                                hospital.assignPatientToStaff(lastSelectedPatientId, inputStaffId);
-                                assignStaffToPatientCardView.setVisibility(View.GONE);
-                            } else {
-                                // TODO TOAST
-                            }
-                        }
-                    });
-                }
-            }
-        });
-
-        this.popUpPatientToAdmissionPhaseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hospital.putPatientIntoPhase(lastSelectedPatientId, 4);
-                popUpMultipleNextPhasesTextView.setVisibility(View.GONE);
-                popUpPatientToAdmissionPhaseButton.setVisibility(View.GONE);
-                popUpPatientToERSepsisTriagePhaseButton.setVisibility(View.GONE);
-            }
-        });
-
-        this.popUpPatientToERSepsisTriagePhaseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hospital.putPatientIntoPhase(lastSelectedPatientId, 2);
-                popUpMultipleNextPhasesTextView.setVisibility(View.GONE);
-                popUpPatientToAdmissionPhaseButton.setVisibility(View.GONE);
-                popUpPatientToERSepsisTriagePhaseButton.setVisibility(View.GONE);
-            }
-        });
-
-        this.assignStaffToPatientCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                assignStaffToPatientCardView.setVisibility(View.GONE);
-            }
-        });
-
-        this.addNewPatientCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addNewPatientCardView.setVisibility(View.GONE);
-            }
-        });
-
-        this.addPatientFloatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (addNewPatientCardView.getVisibility() == View.GONE) {
-                    addNewPatientCardView.setVisibility(View.VISIBLE);
-                } else {
-                    // TODO check if patient id exists already
-                    String newPatientIdInput = enterNewPatientIdEditText.getText().toString();
-                    String newPatientFirstNameInput = enterNewPatientFirstNameEditText.getText().toString();
-                    String newPatientLastNameInput = enterNewPatientLastNameEditText.getText().toString();
-                    if (Math.min(Math.min(newPatientIdInput.length(), newPatientFirstNameInput.length()), newPatientLastNameInput.length()) > 0) {
-                        hospital.addPatient(newPatientIdInput, newPatientFirstNameInput, newPatientLastNameInput);
-                        addNewPatientCardView.setVisibility(View.GONE);
-                    } else {
-                        // TODO TOAST
-                    }
-                }
-            }
-        });
     }
 
+    /**
+     * Show a small pop up to perform operations on the given patient
+     *
+     * @param patientId the id of the patient to perform operations on
+     */
     public void popUpPatientOverview(String patientId) {
         if (!patientId.equals(this.lastSelectedPatientId)) {
             popUpMultipleNextPhasesTextView.setVisibility(View.GONE);
@@ -199,15 +212,17 @@ public class StartActivity extends AppCompatActivity {
         this.lastSelectedPatientId = patientId;
 
         this.popUpPatientOverviewCardView.setVisibility(View.VISIBLE);
-        hospital.subscribeSinglePatient(patientId, new ICallback<Map<String, Object>>() {
-            @Override
-            public void onCallback(Map<String, Object> patient) {
-                popUpPatientNameTextView.setText(patient.get("firstName").toString() + " " + patient.get("lastName").toString());
-            }
-        });
+        hospital.subscribeSinglePatient(patientId, patient -> popUpPatientNameTextView.setText(patient.get("firstName").toString() + " " + patient.get("lastName").toString()));
     }
 
-    public void hidePatientOverview() {
-        this.popUpPatientOverviewCardView.setVisibility(View.GONE);
+    /**
+     * Opens an extra window for detailed patient information
+     *
+     * @param patientId the patients id to display the information of
+     */
+    public void openPatientActivity(String patientId) {
+        Intent intent = new Intent(this, PatientActivity.class);
+        intent.putExtra(EXTRA_ID, patientId);
+        startActivity(intent);
     }
 }
