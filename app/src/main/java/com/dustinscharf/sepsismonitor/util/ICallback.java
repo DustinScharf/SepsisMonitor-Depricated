c8090a0ca3a9ccb5dbf401726362e4680f277e85
DustinScharf <d.scharf.2002@gmail.com>
2021-11-14 01:00:06 +0100

    Added javadoc to ICallback

--- app/src/main/java/com/dustinscharf/sepsismonitor/util/ICallback.java
+++ app/src/main/java/com/dustinscharf/sepsismonitor/util/ICallback.java
@@ -1,5 +1,15 @@
 package com.dustinscharf.sepsismonitor.util;
 
+/**
+ * A callback that can be used to return any data at any event
+ *
+ * @param <T> the type of the data to be returned
+ */
 public interface ICallback<T> {
+    /**
+     * A callback that can be fired at any event
+     *
+     * @param t the data the callback contains when it is fired
+     */
     void onCallback(T t);
 }
