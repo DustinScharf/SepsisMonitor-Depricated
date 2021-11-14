553ae78a5fcb606eee0cecd0a2bb18cd6c746bc6
DustinScharf <d.scharf.2002@gmail.com>
2021-11-14 18:51:49 +0100

    Added toolbar

--- app/src/main/java/com/dustinscharf/sepsismonitor/ui/LoginActivity.java
+++ app/src/main/java/com/dustinscharf/sepsismonitor/ui/LoginActivity.java
@@ -1,25 +1,33 @@
 package com.dustinscharf.sepsismonitor.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.dustinscharf.sepsismonitor.R;
 import com.dustinscharf.sepsismonitor.logic.IHospital;
-import com.dustinscharf.sepsismonitor.util.ICallback;
 
-import java.util.Map;
-
+/**
+ * This activity contains a login area
+ * This includes a name- and password text field as well as a confirm button
+ */
 public class LoginActivity extends AppCompatActivity {
 
+    /**
+     * Builds the initial gui for this activity
+     *
+     * @param savedInstanceState the data from events happened before the start of this activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
 
         IHospital hospital = IHospital.getNewInstance();
 
@@ -27,28 +35,29 @@ public class LoginActivity extends AppCompatActivity {
         EditText passwordEditText = findViewById(R.id.passwordEditText);
 
         Button loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String inputId = idEditText.getText().toString();
-                String inputPassword = passwordEditText.getText().toString();
-                hospital.fetchSingleStaff(inputId, new ICallback<Map<String, Object>>() {
-                    @Override
-                    public void onCallback(Map<String, Object> stringObjectMap) {
-                        boolean loginSuccess = hospital.tryLogin(stringObjectMap, inputId, inputPassword);
-                        if (loginSuccess) {
-                            hospital.setLoggedInStaffId(inputId);
-                            hospital.setLoggedInStaffIsLMMP((Boolean) stringObjectMap.get("isLMMP"));
-                            openStartActivity();
-                        } else {
-                            // TODO TOAST
-                        }
-                    }
-                });
-            }
+        loginButton.setOnClickListener(view -> {
+            String inputId = idEditText.getText().toString().toLowerCase();
+            String inputPassword = passwordEditText.getText().toString();
+            hospital.fetchSingleStaff(inputId, stringObjectMap -> {
+                boolean loginSuccess = hospital.tryLogin(stringObjectMap, inputId, inputPassword);
+                if (loginSuccess) {
+                    hospital.setLoggedInStaffId(inputId);
+                    hospital.setLoggedInStaffIsLMMP((Boolean) stringObjectMap.get("isLMMP"));
+                    openStartActivity();
+                } else {
+                    Toast.makeText(
+                            this,
+                            "Wrong credentials, please try again",
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            });
         });
     }
 
+    /**
+     * Opens the start activity
+     */
     private void openStartActivity() {
         Intent intent = new Intent(this, StartActivity.class);
         startActivity(intent);
--- app/src/main/java/com/dustinscharf/sepsismonitor/ui/MainActivity.java
+++ app/src/main/java/com/dustinscharf/sepsismonitor/ui/MainActivity.java
@@ -2,34 +2,39 @@ package com.dustinscharf.sepsismonitor.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dustinscharf.sepsismonitor.R;
-import com.dustinscharf.sepsismonitor.logic.IHospital;
-import com.dustinscharf.sepsismonitor.util.ICallback;
 
-import java.util.Map;
-
+/**
+ * This activity contains a start screen of the app
+ * with the title and a button to proceed
+ * <p>
+ * The activity is shown when the app is launched
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Builds the initial gui for this activity
+     *
+     * @param savedInstanceState the data from events happened before the start of this activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+
         Button toLoginButton = findViewById(R.id.toLoginButton);
-        toLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openLoginActivity();
-            }
-        });
+        toLoginButton.setOnClickListener(view -> openLoginActivity());
     }
 
+    /**
+     * Opens the login activity
+     */
     private void openLoginActivity() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
